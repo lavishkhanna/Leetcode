cdaac1621ab074ae5f1b7a6d01275495cbb7395b
Lavish Khanna <93002369+lavishkhanna@users.noreply.github.com>
2023-09-11 00:49:18 +0530

    Time: 11 ms (39.37%), Space: 8.2 MB (21.55%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        map<ListNode*,int>omap;
+        ListNode * temp=head;
+        if(head==NULL || head->next==NULL){
+            return false;
+        }
+        ListNode * slow=head;
+        ListNode * fast=head;
+        
+        while(fast && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+
+};
