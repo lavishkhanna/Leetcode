e7039e4c4db638cba3a000e26fd3e1611ab5f3a8
Lavish Khanna <93002369+lavishkhanna@users.noreply.github.com>
2023-09-19 14:40:40 +0530

    Time: 627 ms (5.01%), Space: 104 MB (6.89%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -2,13 +2,10 @@ class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
         map<int,int>omap;
-
         for(int i=1;i<nums.size();i++){
             omap[i]=0;
             cout<<i<<endl;
         }
-
-
         for(int i=0;i<nums.size();i++){
             if(omap.count(nums[i])>0){
                 omap.erase(nums[i]);
